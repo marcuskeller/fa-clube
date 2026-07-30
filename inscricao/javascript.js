2d41ee0fbbe7ee8b61be0e47d523179d24c7b399
marcuskeller <v.marcusk@gmail.com>
2026-07-29 21:14:29 -0300

    Add CPF field and validation to registration form

--- inscricao/javascript.js
+++ inscricao/javascript.js
@@ -1,15 +1,11 @@
-function register(event) {
-    const nome = document.getElementById('nome');
+function register(event) { 
     const sexoSelecionado = document.querySelector('input[name="sexo"]:checked');
     const dataNascimento = document.getElementById('date');
-    const email = document.getElementById('email');
     const checkboxParticipar = document.getElementById('participar');
 
-    if (nome.value.trim() === "") {
-        alert("Por favor, preencha o campo Nome Completo.");
-        nome.focus();
-        return;
-    }
+    verifyNameField()
+
+    verifyCPFFileld()
 
     if (!sexoSelecionado) {
         alert("Por favor, selecione uma opção de Gênero.");
@@ -23,11 +19,7 @@ function register(event) {
         return;
     }
 
-    if (email.value.trim() === "") {
-        alert("Por favor, preencha o campo E-mail.");
-        email.focus();
-        return;
-    }
+    verifyEmailField()
 
     if (!checkboxParticipar.checked) {
         alert("Você precisa concordar com os termos para finalizar a inscrição.");
@@ -35,7 +27,65 @@ function register(event) {
         return;
     }
 
-    alert("Parabéns " + nome.value  + ", inscrição feita com sucesso!");
+    const nome = document.getElementById('nome').value;
+    alert("Parabéns " + nome + ", inscrição feita com sucesso!");
 
     event.target.closest('form').reset();
+}
+
+function verifyNameField(){
+    const nome = document.getElementById('nome');
+    const nomeRegex = new RegExp("^[A-Za-zÀ-ÿ]{2,}([ ][A-Za-zÀ-ÿ]{2,})+$");
+
+    if (nome.value.trim() === "") {
+        alert("Por favor, preencha o campo Nome Completo.");
+        nome.focus();
+        throw new Error("Nome Vazio");
+        return;
+    }
+
+    if (!nomeRegex.test(nome.value)){
+        alert("Nome incorreto! Insira no mínimo 2 caracteres para nome e 2 caracteres para sobrenome.")
+        nome.focus();
+        throw new Error("Nome Inválido");
+        return;
+    }
+}
+
+function verifyCPFFileld(){
+    const CPF = document.getElementById('CPF');
+    const CPFRegex = new RegExp("^[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}$");
+
+    if (CPF.value.trim() === "") {
+        alert("Por favor, preencha o campo de CPF.");
+        CPF.focus();
+        throw new Error("CPF Vazio");
+        return;
+    }
+
+    if (!CPFRegex.test(CPF.value)){
+        alert("CPF incorreto! Insira um CPF válido conforme o exemplo no campo do CPF.");
+        CPF.focus();
+        throw new Error("CPF Inválido");
+        return;
+    }
+}
+
+function verifyEmailField(){
+    const email = document.getElementById('email');
+    const emailRegex = new RegExp("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+
+    if (email.value.trim() === "") {
+        alert("Por favor, preencha o campo E-mail.");
+        email.focus();
+        throw new Error("Email Vazio");
+        return;
+    }
+
+    if (!emailRegex.test(email.value)){
+        alert("Email incorreto! Insira um email válido conforme o exemplo no campo do email.");
+        email.focus();
+        throw new Error("Email Inválido");
+        return;
+    }
 }
